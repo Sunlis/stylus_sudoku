24f54cfa620a52275a02ef5429610d897465df99
Sean Dohring <seandohring@gmail.com>
2026-03-14 15:47:07 -0700

    little baby app

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <main style={{ padding: '1.5rem', fontFamily: 'system-ui, sans-serif' }}>
+      <div>we did it</div>
+    </main>
+  );
+}
+
+export default App;
