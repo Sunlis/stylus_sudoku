b75a2e6d16a1f3edde8003e9af5ff334c352028e
Sean Dohring <seandohring@gmail.com>
2026-03-28 15:08:46 -0700

    PWA - not fully tested

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), basicSsl()],
   server: {
     host: true,
+    https: true,
+  },
+  preview: {
+    https: true,
   },
   test: {
     environment: 'jsdom',
