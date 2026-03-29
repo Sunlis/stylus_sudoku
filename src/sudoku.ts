51ba69f9922b88afb9699a2ce93a6fe3b9abc47d
Sean Dohring <seandohring@gmail.com>
2026-03-29 11:05:57 -0700

    sudoku helper methods

--- src/sudoku.ts
+++ src/sudoku.ts
@@ -1,21 +1,74 @@
 import { CellContents } from "@app/types/board";
 
-export function isBoardValid(board: CellContents[][]): boolean {
-  // Check rows and columns
-  for (let i = 0; i < 9; i++) {
-    if (!isRowValid(board, i) || !isColumnValid(board, i)) {
-      return false;
+/**
+ * Run the callback for each "group" in the board (row, column, or box).
+ * If the callback returns true, iteration will stop early.
+ */
+export function forEachGroup(board: CellContents[][], callback: (group: CellContents[]) => boolean | void): void {
+  // Rows
+  for (let row = 0; row < 9; row++) {
+    const rowGroup = board[row];
+    if (callback(rowGroup)) {
+      return;
     }
   }
-  // Check boxes
+
+  // Columns
+  for (let col = 0; col < 9; col++) {
+    const colGroup = [];
+    for (let row = 0; row < 9; row++) {
+      colGroup.push(board[row][col]);
+    }
+    if (callback(colGroup)) {
+      return;
+    }
+  }
+
+  // Boxes
   for (let boxRow = 0; boxRow < 3; boxRow++) {
     for (let boxCol = 0; boxCol < 3; boxCol++) {
-      if (!isBoxValid(board, boxRow, boxCol)) {
-        return false;
+      const boxGroup = [];
+      for (let row = boxRow * 3; row < boxRow * 3 + 3; row++) {
+        for (let col = boxCol * 3; col < boxCol * 3 + 3; col++) {
+          boxGroup.push(board[row][col]);
+        }
+      }
+      if (callback(boxGroup)) {
+        return;
       }
     }
   }
-  return true;
+}
+
+export function forEachCell(board: CellContents[][], callback: (cell: CellContents, row: number, col: number) => void): void {
+  for (let row = 0; row < 9; row++) {
+    for (let col = 0; col < 9; col++) {
+      callback(board[row][col], row, col);
+    }
+  }
+}
+
+export function boxIndex(row: number, col: number): number {
+  const boxRow = Math.floor(row / 3);
+  const boxCol = Math.floor(col / 3);
+  return boxRow * 3 + boxCol;
+}
+
+export function isBoardValid(board: CellContents[][]): boolean {
+  let valid = true;
+  forEachGroup(board, (group) => {
+    const seen = new Set<number>();
+    for (const cell of group) {
+      if (cell.value !== undefined) {
+        if (seen.has(cell.value)) {
+          valid = false;
+          return true; // Group is invalid, stop iteration.
+        }
+        seen.add(cell.value);
+      }
+    }
+  });
+  return valid;
 }
 
 export function isRowValid(board: CellContents[][], row: number): boolean {
@@ -64,17 +117,12 @@ export function isBoxValid(board: CellContents[][], boxRow: number, boxCol: numb
 
 // Just naively fill all empty cells with all 9 numbers.
 function fillAllCandidates(board: CellContents[][]): CellContents[][] {
-  return board.map((rowArr, row) => {
-    return rowArr.map((cell, col) => {
-      if (cell.value !== undefined) {
-        return cell;
-      }
-      return {
-        ...cell,
-        candidates: [1, 2, 3, 4, 5, 6, 7, 8, 9],
-      };
-    });
+  forEachCell(board, (cell, row, col) => {
+    if (cell.value === undefined) {
+      cell.candidates = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    }
   });
+  return board;
 }
 
 // Clear the given cell's number from all relevant candidates.
@@ -84,36 +132,14 @@ export function clearRelatedCandidates(board: CellContents[][], cellRow: number,
     return board;
   }
 
-  // Row
-  for (let col = 0; col < 9; col++) {
-    const cell = board[cellRow][col];
+  forEachCell(board, (cell, row, col) => {
     if (!cell.candidates) {
-      continue;
+      return;
     }
-    cell.candidates = cell.candidates.filter((candidate) => candidate !== value);
-  }
-
-  // Column
-  for (let row = 0; row < 9; row++) {
-    const cell = board[row][cellCol];
-    if (!cell.candidates) {
-      continue;
-    }
-    cell.candidates = cell.candidates.filter((candidate) => candidate !== value);
-  }
-
-  // Box
-  const boxRow = Math.floor(cellRow / 3);
-  const boxCol = Math.floor(cellCol / 3);
-  for (let row = boxRow * 3; row < boxRow * 3 + 3; row++) {
-    for (let col = boxCol * 3; col < boxCol * 3 + 3; col++) {
-      const cell = board[row][col];
-      if (!cell.candidates) {
-        continue;
-      }
+    if (cellRow == row || cellCol == col || boxIndex(cellRow, cellCol) == boxIndex(row, col)) {
       cell.candidates = cell.candidates.filter((candidate) => candidate !== value);
     }
-  }
+  });
 
   return board;
 }
